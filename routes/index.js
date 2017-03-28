99c1e0eb7bcdc1707e26e0f0168c767b6eb77b93
Lucas <dlucasgf@gmail.com>
2017-03-28 13:52:11 -0300

    Testando o sleep

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
+var sleep = require('sleep');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  var i = 0;
+  while (i < 10) {
+
+    console.log('Why so serious? ' + i);
+    sleep.msleep(500);
+
+    i++;
+  }
+  
   res.render('index', { title: 'Express' });
 });
 

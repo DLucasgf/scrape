3122a291f347e2ab3c8d0d01b4ce6f0482c17adc
Lucas <dlucasgf@gmail.com>
2017-03-31 13:51:09 -0300

    Código de teste

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var sleep = require('sleep');
+var url = require('url');
 var router = express.Router();
 
 /* GET home page. */
@@ -8,12 +9,22 @@ router.get('/', function(req, res, next) {
   var i = 0;
   while (i < 10) {
 
-    console.log('Why so serious? ' + i);
-    sleep.msleep(500);
+    //console.log('Why so serious? ' + i);
+    //sleep.msleep(500);
 
     i++;
   }
+
+  var articleUrl = "http://www.scielo.org.mx/scielo.php?script=sci_arttext&pid=S2007-249X2016000200060&lang=pt";
   
+
+  var tmp = url.parse(articleUrl, true);
+  console.log('tmp');
+  console.log(tmp);
+  var pid = tmp.query.pid;
+  console.log('pid');
+  console.log(pid);
+
   res.render('index', { title: 'Express' });
 });
 
